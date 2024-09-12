5f579fa64574f1346bf5d61efcbecbee814f999b
NickSandelRWH <nick.sandel@realworld.health>
2024-09-12 13:24:17 +0100

    Applied black formatting

--- dsptools/data_transfer/savetosql.py
+++ dsptools/data_transfer/savetosql.py
@@ -1,6 +1,15 @@
 from bcpandas import SqlCreds, to_sql
 
-def dataframe_to_sql_bcpandas(engine, dataframe, table_name, dtype=None, schema="dbo", if_exists='replace', index=False):
+
+def dataframe_to_sql_bcpandas(
+    engine,
+    dataframe,
+    table_name,
+    dtype=None,
+    schema="dbo",
+    if_exists="replace",
+    index=False,
+):
     """
     A wrapper around bcpandas to_sql which is a wrapper around pandas DataFrame.to_sql
 
@@ -14,5 +23,12 @@ def dataframe_to_sql_bcpandas(engine, dataframe, table_name, dtype=None, schema=
         index (optional): Write DataFrame index as a column. Uses the index name as the column name in the table.
     """
     creds = SqlCreds.from_engine(engine)
-    to_sql(df=dataframe, table_name=table_name, creds=creds, dtype=dtype, schema=schema, if_exists=if_exists, index=index)
-
+    to_sql(
+        df=dataframe,
+        table_name=table_name,
+        creds=creds,
+        dtype=dtype,
+        schema=schema,
+        if_exists=if_exists,
+        index=index,
+    )
